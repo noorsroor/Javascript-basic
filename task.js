8f108239c59e4c6e60694490afaf3cd6d8d33f3f
noorsroor <noorsroor67@gmail.com>
2024-12-26 16:20:48 +0300

    task file

---
+++ task.js
@@ -0,0 +1,43 @@
+let x =4.3;
+
+console.log("Q1")
+console.log("1. "+ -5*3);
+console.log("2. "+ "JavaScript " + 50);
+console.log("3. "+17 % 5);
+console.log("4. "+5 % 17);
+console.log("5. "+5/10);
+console.log("6. "+( 4 === '4'));
+console.log("7. "+( 4 != 5));
+console.log("8. "+ (7 <= 8));
+console.log("9. "+ "Hello" + 5);
+console.log("10. "+( Math.ceil(x) - Math.floor(x)));
+console.log("11. "+ Math.pow(x,2));
+
+console.log("Q2");
+let y=prompt("Enter a number","1");
+window.alert("the number is: "+ y);
+
+console.log("Q3");
+let n1=parseInt(prompt("Enter the first number","1"));
+let n2=parseInt(prompt("Enter the second number","1"));
+window.alert("The sum of numbers: " +(n1+n2));
+
+console.log("Q4");
+let n3=parseInt(prompt("Enter the first number","1"));
+let n4=parseInt(prompt("Enter the second number","1"));
+window.alert("The product of numbers: " +(n1*n2));
+
+console.log("Q5");
+let n5=parseInt(prompt("Enter the first number","1"));
+let n6=parseInt(prompt("Enter the second number","1"));
+window.alert("The dividing of numbers: " +(n1/n2));
+
+console.log("Q6");
+window.alert("the sum of 5+10= "+ (5+10));
+
+
+
+
+
+
+
